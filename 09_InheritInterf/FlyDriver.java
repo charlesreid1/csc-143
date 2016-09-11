4a6fe5b1909862e35b191a2f4c264cc02254d0f7
Charles Reid <charlesreid1@gmail.com>
2016-09-11 15:47:25 -0700

    adding inheritance example for lecture 9

---
+++ 09_InheritInterf/FlyDriver.java
@@ -0,0 +1,6 @@
+public class FlyDriver {
+    public static void main(String[] args) {
+        Subfly s = new Subfly(3);
+        s.hello();
+    }
+}
---
+++ 09_InheritInterf/Superfly.java
@@ -0,0 +1,6 @@
+public class Superfly {
+    private int a;
+    public Superfly( int a ) {
+        this.a = a;
+    }
+}
