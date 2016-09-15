bee9048a216f39d7c26b1765469fa5272cfafc2d
Charles Reid <charlesreid1@gmail.com>
2016-09-14 19:47:35 -0700

    updating turtle driver

--- 09_InheritInterf/turtle/TurtleDriver.java
+++ 09_InheritInterf/turtle/TurtleDriver.java
@@ -4,10 +4,12 @@ public class TurtleDriver {
     public static void main(String[] args) { 
         Turtle tim = new Turtle();
 
-        int size = 10;
-        for( int i = 1; i <= 4; i++ ) {
-            tim.forward(size);
-            tim.right(90);
+        for( int size = 10; size <= 60; size += 10 ) {
+            for( int j = 1; j <= 4; j++ ) {
+                tim.forward(size);
+                tim.right(90);
+            }
         }
+
     }
 }
