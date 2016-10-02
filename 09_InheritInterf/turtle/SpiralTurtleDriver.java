bba60e855dc540e781526e844792919969f71875
Charles Reid <charlesreid1@gmail.com>
2016-10-02 10:16:49 -0700

    adding spiral turtle driver for worksheet

---
+++ 09_InheritInterf/turtle/SpiralTurtleDriver.java
@@ -0,0 +1,13 @@
+import essential.*;
+
+public class SpiralTurtleDriver {
+    public static void main(String[] args) { 
+
+        SpiralTurtle tim = new SpiralTurtle();
+        tim.drawSpiral();
+
+        SpiralTurtle tom = new SpiralTurtle(0.5);
+        tom.drawSpiral();
+
+    }
+}
