8514d3889f22e6678b246fa500cc08fb69faf5cb
Charles Reid <charlesreid1@gmail.com>
2016-10-12 23:32:35 -0700

    updating polynomial and polynomial driver classes

--- 00_HelloWorld/Polynomial.java
+++ 00_HelloWorld/Polynomial.java
@@ -1,11 +1,20 @@
-/*
-Polynomial class
-
-Polynomial coefficients are indexed by degree
-
-coefficients[0] = coefficient of x^0
-coefficients[1] = coefficient of x^1
-coefficients[2] = coefficient of x^2
+/**
+ * Polynomial
+ * Worksheet 1 
+ * Dr. Reid
+ *
+ * This class implements a Polynomial as an object
+ * using an array for storing information about the 
+ * coefficients. It defines several useful features:
+ * - copy constructor
+ * - polymorphic add method (Polynomials or integers or doubles)
+ *
+ * Polynomial coefficients are stored as an array
+ * and are indexed by degree:
+ *
+ * coefficients[0] = coefficient of x^0
+ * coefficients[1] = coefficient of x^1
+ * coefficients[2] = coefficient of x^2
 */
 public class Polynomial {
     private int degree;
@@ -24,16 +33,17 @@ public class Polynomial {
         // Note: we can't just say this.coefficients = p.coefficients, 
         // b/c then they would point to the same array in memory 
         // (that would be a shallow copy)
+        int len = p.degree-1;
         this.coefficients = new double[len];
-        for(int i=0; i<p.coefficients.length; i++){
-            newcoeff[i] = p.coefficients[i];
+        for(int i=0; i<len; i++) {
+            this.coefficients[i] = p.coefficients[i];
         }
     }
     
-    pubilc Polynomial add(int a) { 
+    public Polynomial add(double a) { 
         Polynomial result = new Polynomial(this);
         // add x to the degree 0 term
-        result.coeffs[0] += a;
+        result.coefficients[0] += a;
         return result;
     }
 
@@ -41,27 +51,35 @@ public class Polynomial {
         // Add two polynomials:
         //   r = p + q
         
-        if( q.degree > this.degree ) {
-            Polynomial r = new Polynomial(q);
-        } else {
-            Polynomial r = new Polynomial(this);
-        
-        // Add coeffs from p
-        for( int d = 0; d <= this.degree; d++ ) {
-            r.coefficients[d] = this.coefficients[d];
-        }
+        Polynomial r;
 
-        // Add coeffs from q
-        for( int d = 0; d <= q.degree; d++ ) { 
-            r.coefficients[d] += q.coefficients[d];
+        if( q.degree > this.degree ) {
+
+            r = new Polynomial(q);
+
+            // Add coeffs from p
+            for( int d = 0; d < this.degree; d++ ) {
+                r.coefficients[d] = this.coefficients[d];
+            }
+
+        } else {
+
+            r = new Polynomial(this);
+
+            // Add coeffs from q
+            System.out.println(q);
+            for( int d = 0; d < q.degree; d++ ) { 
+                r.coefficients[d] += q.coefficients[d];
+            }
+
         }
 
         return r;
     }
 
-    public String toString() {
+    public String toString() { 
         String r = "";
-        for( int d = this.degree; d >= 0; d-- ) {
+        for( int d = this.coefficients.length-1; d >= 0; d-- ) {
             r += Math.abs(this.coefficients[d]);
             if( d > 0 ) {
                 r += " x";
--- 00_HelloWorld/PolynomialDriver.java
+++ 00_HelloWorld/PolynomialDriver.java
@@ -32,24 +32,20 @@ public class PolynomialDriver {
         1, 0, 0
         9
         */
+
+        // Be careful! These arrays are listed in reverse order, 
+        // so that element 0 corresponds to x^0,
+        // element 1 corresponds to x^1, etc.
+        // So, you have to reverse the normal order of the coefficients.
+        //
+        // 1a 
+        Polynomial p1 = new Polynomial(new double[] {10, -1, 2});
+        //Polynomial p2 = new Polynomial(new double[] {9, 0, -1});
+        //Polynomial p3 = p1.add(p2);
+        System.out.println(p1);
+        //System.out.println(p2);
+        //System.out.println(p3);
         
-        double[] c1 = new double[4];
-        // 4 x^3 + x^2 + 4 x + 1
-        c1[3] = 4.0;
-        c1[2] = 1.0;
-        c1[1] = 4.0;
-        c1[0] = 1.0;
-        Polynomial p1 = new Polynomial(3,c1);
-
-        // x^2 + 2x + 1
-        double[] c2 = {1.0, 2.0, 1.0};
-        Polynomial p2 = new Polynomial(2,c2);
-
-        Polynomial p3 = new Polynomial(2,new double[] {-2.0,1.0,1.0});
-        Polynomial s = p2.add(p3);
-        System.out.println(s);
-
-        //Polynomial r = p1.add(p2);
     }
 }
 
