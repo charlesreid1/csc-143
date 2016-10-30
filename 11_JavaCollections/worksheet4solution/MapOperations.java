4cfba2129800d9fdb38db26f885f1a9e6b39d118
C Reid <charlesreid1@Gmail.com>
2016-10-30 00:23:36 -0700

    Update MapOperations.java

--- 11_JavaCollections/worksheet4solution/MapOperations.java
+++ 11_JavaCollections/worksheet4solution/MapOperations.java
@@ -1,7 +1,12 @@
 import java.util.*;
 
+/** MapOperations:
+ *
+ * This class defines several static methods for reversing maps.
+ */
 public class MapOperations { 
 
+    /** Reverse a String-String map */
     public static TreeMap<String, String> reverseMap( TreeMap<String, String> forward ) {
         TreeMap<String,String> reverse = new TreeMap<String,String>();
         for( String key : forward.keySet() ) {
@@ -11,6 +16,7 @@ public class MapOperations {
         return reverse;
     }
 
+    /** Reverse a String-Double map */
     public static TreeMap<Double, String> reverseDistanceMap( TreeMap<String, Double> forward ) {
         TreeMap<Double,String> reverse = new TreeMap<Double,String>();
         for( String key : forward.keySet() ) {
