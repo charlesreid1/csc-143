92aea787727a43fe591d753b0880c3df557dc854
Charles Reid <charlesreid1@gmail.com>
2016-11-12 20:14:14 -0800

    adding an example of noise/distortion

---
+++ 14_StackQueue/Distortion.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+// Heavily modified from http://courses.cs.washington.edu/courses/cse143/14wi/homework/ass2/PlayThatTune.java.html
+
+public class Distortion {
+
+    public static void main(String[] args) {
+
+        Random r = new Random();
+        while (true) { 
+
+            // read in the pitch, where 0 = Concert A (A4)
+            int pitch = -r.nextInt(6);
+
+            // read in duration in seconds
+            double duration = 2.0;
+
+            // build sine wave with desired frequency
+            double hz  = 440 * Math.pow(2, pitch / 12.0);
+
+            int N = (int) (StdAudio.SAMPLE_RATE * duration);
+            double[] a = new double[N+1];
+
+            for (int i = 0; i <= N; i++) {
+                a[i]  = Math.sin(2 * Math.PI * i * hz  / StdAudio.SAMPLE_RATE);
+
+                a[i] += Math.random() * ((double)(N-i))/N;
+
+            }
+
+            // play it using standard audio
+            StdAudio.play(a);
+        }
+    }
+}
